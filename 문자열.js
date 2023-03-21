7f5919b8a9d4d87d54dcbde63f5672bd55b97707
ansrlgur12 <ansrlgur12@naver.com>
2023-03-21 14:38:28 +0900

    0321 커밋

--- 문자열.js
+++ 문자열.js
@@ -21,7 +21,17 @@ console.log(인사말);
 let string22 = "\n\n\n이스케이프 문자를 이용해서\n줄 바꿈하고 싶어요";
 console.log(string22);
 
+// ES6에서 추가된 템플릿 문자열 사용하기
 let dan = 3;
 let gugu = 8;
 let rst = `${dan} 곱하기 ${gugu}은 ${dan * gugu}입니다.`;
-console.log(rst);
+console.log(rst); 
+
+// 숫자형 (Number) : 자바스크립트는 정수와 실수를 구분하지 않고 전부 하나의 숫자 자료형으로 취급 합니다.
+let num1 = 10;
+let num2 = 0.1;
+console.log(num1 + num2);
+console.log(num2);
+
+let sum = 0.1 + 0.2;
+console.log(sum);
