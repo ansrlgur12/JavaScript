6c2935aa97b3d2648d301c7a2ab99a602186e750
ansrlgur12 <ansrlgur12@naver.com>
2023-03-23 11:09:59 +0900

    0323

--- 주민번호.js
+++ 주민번호.js
@@ -2,20 +2,26 @@
 // 성별 확인 123456-1234567
 // 현재 년도 - 태어난 년도 = 나이
 
-
+let num 
 let gender;
 let age;
 let year = 2023;
 const date = new Date();
 while(true){
-    let num = prompt("주민 등록 번호를 입력하세요 : ", "");
+    num = prompt("주민 등록 번호를 입력하세요 : ", "");
+    if(num.length != 14) alert("입력 오류 : 다시 입력하세요");
+    else {
+        if(num.indexOf("-") == -1) alert("하이픈이 누락되었습니다.");
+        else break;
+    }
+}
+
     if(num.charAt(7) == 1 || num.charAt(7) == 3) gender = "남자";
     else if(num.charAt(7) == 2 || num.charAt(7) == 4) gender = "여자";
-    else document.write("잘못입력했습니다."); break;
+    else document.write("잘못입력했습니다.");
 
     age = Number(num.substring(0,2));
     if(num.charAt(7) == 1 || num.charAt(7) == 2) age += 1900;
     age = year - age;
     document.write(`<h2>성별은 ${gender}</h2>`);
     document.write(`<h2>나이는 ${age}</h2>`);
-}
